6fd833262543a7fa86ea14f4263d75d1899fb27e
mariana kolodgeeva <Kolodgeeva@vistajet.com>
2018-05-31 14:05:35 +0300

    Js functionality

--- public/main.js
+++ public/main.js
@@ -2,132 +2,123 @@
 
   "use strict";
 
-  let content = document.getElementById('content');
+  const body = document.getElementById('body');
 
-  content.addEventListener('click', (e) => {
+  body.addEventListener('click', (e) => {
     if (e.target.classList.contains('add-list-btn')) {
+      e.preventDefault();
+      openSidebar();
+    }
+  });
+
+  body.addEventListener('click', (e) => {
+    if (e.target.classList.contains('close-sidebar')) {
+      e.preventDefault();
+      closeSidebar();
+    }
+  });
+
+  body.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add-btn')) {
       e.preventDefault();
       addTodoList();
     }
   });
 
-  content.addEventListener('click', (e) => {
+  body.addEventListener('click', (e) => {
     if (e.target.classList.contains('remove-list-btn')) {
       e.preventDefault();
       removeTodoList(e);
     }
   });
 
-  content.addEventListener('keypress', (e) => {
+  body.addEventListener('keypress', (e) => {
     if (e.target.classList.contains('add-item-btn') && e.key === 'Enter') {
       e.preventDefault();
       addTodoItem(e);
+      this.focus();
     }
   });
 
-  let addTodoList = () => {
-    document.getElementById("main").appendChild(createElement({
-      element: "div",
-      classes: ["todo-block"],
-      children: [
-        createElement({
-          element: "div",
-          classes: ["todo-header"],
-          children: [
-            createElement({
-              element: "div",
-              classes: ["todo-header-bar"],
-              children: [createElement({
-                element: "button",
-                classes: ["remove-list-btn"]
-              })]
-            }),
-            createElement({
-              element: "div",
-              classes: ["todo-header-content"],
-              children: [createElement({
-                element: "label",
-                children: [createElement({
-                  element: "input",
-                  type: "text",
-                  placeholder: "TODO List Name"
-                })]
-              })]
-            })
-          ]
-        }),
-        createElement({
-          element: "ul",
-          classes: ["todo-list"],
-          children: [createElement({
-            element: "li",
-            children: [createElement({
-              element: "label",
-              children: [createElement({
-                element: "input",
-                type: "text",
-                placeholder: "Add to-do",
-                classes: ["add-item-btn", "label"]
-              })]
-            })]
-          })]
-        })
-      ]
-    }));
+  const openSidebar = () => {
+    document.getElementById("sidebar").style.right = "0";
   };
 
-  let removeTodoList = (e) => {
+  const closeSidebar = () => {
+    document.getElementById("sidebar").style.right = "-350px";
+  };
+
+  const addTodoList = () => {
+    closeSidebar();
+
+    document.getElementById("main").innerHTML += Object.create(TodoListBlock)
+      .build(
+          document.getElementById("new-todo-title").value,
+          Array.from(
+              document.querySelectorAll('#new-todo-items>li>label>span.label')
+          ).map((span) => {return span.textContent;})
+      );
+
+    document.getElementById("new-todo-title").value = '';
+
+    Array.from(
+        document.querySelectorAll('#new-todo-items>li.li-item')
+    ).map((li) => {li.parentNode.removeChild(li)})
+  };
+
+  const TodoListBlock = {
+    title: "",
+    items: [],
+    build: (title, items) => {
+      this.title = title;
+      this.items = items;
+      return `
+        <div class="todo-block">
+          <div class="todo-header">
+            <div class="todo-header-bar">
+              <button class="remove-list-btn"></button>
+            </div>
+            <div class="todo-header-content">
+              <label>
+                <input type="text" placeholder="Add title" value="${this.title}">
+              </label>
+            </div>
+          </div>
+          <ul class="todo-list">
+            ${this.items.map((item) => Object.create(TodoListItem).build(item)).join('')}
+            <li><label>
+              <input class="add-item-btn label" type="text" placeholder="Add to-do">
+            </label></li>
+          </ul>
+        </div>`
+    }
+  };
+
+  const TodoListItem = {
+    title: "",
+    build: (title) => {
+      this.title = title;
+      return `
+        <li class="li-item">
+          <label class="checkbox-container">
+            <input type="checkbox">
+            <span class="label">${this.title}</span>
+            <span class="check-mark"></span>
+          </label>
+        </li>         
+      `
+    }
+  };
+
+  const removeTodoList = (e) => {
     e.target.closest(".todo-block").style.display = "none";
   };
 
-  let addTodoItem = (e) => {
+  const addTodoItem = (e) => {
     if (e.target.value.trim()) {
-      e.target.closest("ul").insertBefore(
-          createElement({
-            element: "li",
-            children: [createElement({
-              element: "label",
-              classes: ["checkbox-container"],
-              children: [
-                createElement({element: "input", type: "checkbox"}),
-                createElement({element: "span", text: e.target.value, classes: ["label"]}),
-                createElement({element: "span", classes: ["check-mark"]})]
-            })]
-          }),
-          e.target.closest("li")
-      );
-      e.target.value = "";
+      e.target.closest("ul").innerHTML =
+          Object.create(TodoListItem).build(e.target.value) + e.target.closest("ul").innerHTML;
     }
   };
-
-  let createElement = (props) => {
-    let element = document.createElement(props.element);
-
-    if (props.children) {
-      props.children.map(function (child) {
-        return element.appendChild(child);
-      });
-    }
-
-    if (props.classes) {
-      props.classes.map(function (className) {
-        return element.classList.add(className);
-      });
-    }
-
-    if (props.type) {
-      element.type = props.type;
-    }
-
-    if (props.placeholder) {
-      element.placeholder = props.placeholder;
-    }
-
-    if (props.text) {
-      element.textContent = props.text;
-    }
-
-    return element;
-  }
 
 })();
